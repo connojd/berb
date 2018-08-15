9eec31fd48ecbd0b8290afa3efb28ce87a8a2238
Connor Davis <davisc@ainfosec.com>
2018-08-15 09:41:37 -0600

    Add missing export
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -7,5 +7,5 @@ sudo apt install build-essential bison flex python bc
 pushd $HOME
 wget https://cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.tar.gz
 tar xzf cmake-3.12.1-Linux-x86_64.tar.gz
-echo 'PATH="$HOME/cmake-3.12.1-Linux-x86_64/bin:$PATH"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/cmake-3.12.1-Linux-x86_64/bin:$PATH"' >> $HOME/.bashrc
 popd
