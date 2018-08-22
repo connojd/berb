42e29dc445c7bc82fdf96cc0cf124defec2ae7f3
Connor Davis <davisc@ainfosec.com>
2018-08-22 12:03:24 -0600

    Integrate buildroot into buildsystem
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- scripts/cmake/config/default.cmake
+++ scripts/cmake/config/default.cmake
@@ -1,5 +1,5 @@
 #
-# Bareflank Hypervisor
+# Havoc Hypervisor
 # Copyright (C) 2018 Assured Information Security, Inc.
 #
 # This library is free software; you can redistribute it and/or
@@ -44,58 +44,96 @@ set(HAVOC_SRC_CMAKE_DEPENDS_DIR ${HAVOC_SRC_CMAKE_DIR}/depends
 # Links
 # ------------------------------------------------------------------------------
 
-set(LINUX_URL "https://github.com/torvalds/linux/archive/v4.18.tar.gz"
+#set(LINUX_URL "https://github.com/torvalds/linux/archive/v4.18.tar.gz"
+#    CACHE INTERNAL FORCE
+#    "Linux URL"
+#)
+#
+#set(LINUX_URL_MD5 "e7d9bb36da7aee08eb1f07a54a2754bd"
+#    CACHE INTERNAL FORCE
+#    "Linux URL MD5 hash"
+#)
+
+set(BUILDROOT_URL "https://github.com/connojd/buildroot/archive/master.zip"
     CACHE INTERNAL FORCE
-    "Linux URL"
+    "Buildroot URL"
 )
 
-set(LINUX_URL_MD5 "e7d9bb36da7aee08eb1f07a54a2754bd"
+set(BUILDROOT_URL_MD5 "16cdf6cced63c65bbc5006d398a6d4ec"
     CACHE INTERNAL FORCE
-    "Linux URL MD5 hash"
+    "Buildroot URL MD5 hash"
 )
 
 # ------------------------------------------------------------------------------
 # Linux guest configs
 # ------------------------------------------------------------------------------
 
+#add_config(
+#    CONFIG_NAME LINUX_BUILD_DIR
+#    CONFIG_TYPE STRING
+#    DEFAULT_VAL ${DEPENDS_DIR}/linux/${USERSPACE_PREFIX}/build
+#    DESCRIPTION "The build directory for the guest kernel"
+#)
+#
+#add_config(
+#    CONFIG_NAME LINUX_CONFIG_IN
+#    CONFIG_TYPE STRING
+#    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/linux/tiny.config.in
+#    DESCRIPTION "The .config file for the guest kernel"
+#)
+#
+#add_config(
+#    CONFIG_NAME LINUX_INITRAMFS_IN
+#    CONFIG_TYPE STRING
+#    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/initramfs/tiny.config.in
+#    DESCRIPTION "The input to the linux tree's usr/gen_init_cpio binary"
+#)
+#
+#add_config(
+#    CONFIG_NAME LINUX_INITRAMFS_ROOT
+#    CONFIG_TYPE STRING
+#    DEFAULT_VAL ${HAVOC_SRC_ROOT_DIR}/initramfs/tiny/
+#    DESCRIPTION "The root of the initramfs to build into the kernel"
+#)
+#
+#add_config(
+#    CONFIG_NAME LINUX_INITRAMFS_IMAGE
+#    CONFIG_TYPE STRING
+#    DEFAULT_VAL ""
+#    DESCRIPTION "Path to an existing initramfs image fit for the guest kernel"
+#)
+#
+#add_config(
+#    CONFIG_NAME EMBED_INITRAMFS
+#    CONFIG_TYPE BOOL
+#    DEFAULT_VAL ON
+#    DESCRIPTION "Embed the initramfs into the kernel image"
+#)
+
 add_config(
-    CONFIG_NAME LINUX_BUILD_DIR
+    CONFIG_NAME BUILDROOT_BUILD_DIR
     CONFIG_TYPE STRING
-    DEFAULT_VAL ${DEPENDS_DIR}/linux/${USERSPACE_PREFIX}/build
-    DESCRIPTION "The build directory for the guest kernel"
+    DEFAULT_VAL ${DEPENDS_DIR}/buildroot/${USERSPACE_PREFIX}/build
+    DESCRIPTION "The build directory for the guest image"
 )
 
 add_config(
-    CONFIG_NAME LINUX_CONFIG_IN
-    CONFIG_TYPE STRING
-    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/linux/tiny.config.in
-    DESCRIPTION "The .config file for the guest kernel"
+    CONFIG_NAME BUILDROOT_CONFIG_IN
+    CONFIG_TYPE FILEPATH
+    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/buildroot/xenstore.config.in
+    DESCRIPTION "The .config file used by buildroot to build the guest image"
 )
 
 add_config(
-    CONFIG_NAME LINUX_INITRAMFS_IN
-    CONFIG_TYPE STRING
-    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/initramfs/tiny.config.in
-    DESCRIPTION "The input to the linux tree's usr/gen_init_cpio binary"
-)
-
-add_config(
-    CONFIG_NAME LINUX_INITRAMFS_ROOT
-    CONFIG_TYPE STRING
-    DEFAULT_VAL ${HAVOC_SRC_ROOT_DIR}/initramfs/tiny/
-    DESCRIPTION "The root of the initramfs to build into the kernel"
-)
-
-add_config(
-    CONFIG_NAME LINUX_INITRAMFS_IMAGE
+    CONFIG_NAME BUILDROOT_ROOTFS_OVERLAY
     CONFIG_TYPE STRING
     DEFAULT_VAL ""
-    DESCRIPTION "Path to an existing initramfs image fit for the guest kernel"
+    DESCRIPTION "The rootfs to overlay onto the guest's rootfs"
 )
 
 add_config(
-    CONFIG_NAME EMBED_INITRAMFS
-    CONFIG_TYPE BOOL
-    DEFAULT_VAL ON
-    DESCRIPTION "Embed the initramfs into the kernel image"
+    CONFIG_NAME LINUX_CONFIG
+    CONFIG_TYPE FILEPATH
+    DEFAULT_VAL ${HAVOC_SRC_CONFIG_DIR}/linux/xenstore.config
+    DESCRIPTION "The .config file for the guest kernel"
 )
---
+++ scripts/cmake/depends/buildroot.cmake
@@ -0,0 +1,94 @@
+#
+# Havoc Hypervisor
+# Copyright (C) 2018 Assured Information Security, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+message(STATUS "Including dependency: buildroot")
+
+download_dependency(
+    buildroot
+    URL         ${BUILDROOT_URL}
+    URL_MD5     ${BUILDROOT_URL_MD5}
+)
+
+if(WIN32 OR CYGWIN OR NOT ENABLE_BUILD_VMM)
+    return()
+endif()
+
+#
+# This must come before configure_file. If an existing image path is
+# set by the user, it takes priority over any list file.
+#
+#
+#if(EXISTS ${BUILDROOT_INITRAMFS_IMAGE})
+#    set(BUILDROOT_INITRAMFS_SOURCE ${BUILDROOT_INITRAMFS_IMAGE})
+#    file(APPEND ${BUILDROOT_INITRAMFS_IN} "")
+#else()
+#    configure_file(
+#        ${BUILDROOT_INITRAMFS_IN}
+#        ${BUILDROOT_BUILD_DIR}/.initramfs.list
+#        @ONLY
+#        NEWLINE_STYLE UNIX
+#    )
+#    set(BUILDROOT_INITRAMFS_SOURCE ${BUILDROOT_BUILD_DIR}/.initramfs.list)
+#endif()
+#
+#if(NOT ${EMBED_INITRAMFS})
+#    set(BUILDROOT_INITRAMFS_SOURCE "")
+#endif()
+#
+#
+# Now that initramfs source is set, we configure .config.in -> .config
+# for the linux build and add the dependency.
+#
+
+configure_file(
+    ${BUILDROOT_CONFIG_IN}
+    ${BUILDROOT_BUILD_DIR}/.config
+    @ONLY
+    NEWLINE_STYLE UNIX
+)
+
+#add_custom_command(
+#    OUTPUT ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.much-kludge
+#    COMMAND ${CMAKE_COMMAND} -E touch ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.much-kludge
+#    COMMAND ${CMAKE_COMMAND} -E touch ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update
+#    DEPENDS ${BUILDROOT_CONFIG_IN}
+#)
+#add_custom_target(
+#    stamp-much-kludge
+#    COMMAND ""
+#    DEPENDS ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.much-kludge
+#)
+#
+#add_custom_command(
+#    OUTPUT ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.very-kludge
+#    COMMAND ${CMAKE_COMMAND} -E touch ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.very-kludge
+#    COMMAND ${CMAKE_COMMAND} -E touch ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update
+#    DEPENDS ${BUILDROOT_INITRAMFS_IN}
+#)
+#add_custom_target(
+#    stamp-very-kludge
+#    COMMAND ""
+#    DEPENDS ${BUILDROOT_BUILD_DIR}/../stamp/linux_${USERSPACE_PREFIX}-update.very-kludge
+#)
+
+add_dependency(
+    buildroot userspace
+    CONFIGURE_COMMAND make CC=gcc O=${BUILDROOT_BUILD_DIR} -C ${CACHE_DIR}/buildroot olddefconfig
+    BUILD_COMMAND     make CC=gcc O=${BUILDROOT_BUILD_DIR} -C ${CACHE_DIR}/buildroot
+    INSTALL_COMMAND   /bin/true #sudo make O=${BUILDROOT_BUILD_DIR} -C ${CACHE_DIR}/linux modules_install
+)
