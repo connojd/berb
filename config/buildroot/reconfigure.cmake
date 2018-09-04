75687cb0a61da2a6d4bbe2d19794bd9665e0fa0f
Connor Davis <davisc@ainfosec.com>
2018-09-03 22:46:13 -0600

    Add reconfigure step for buildroot
    We configure the buildroot config file through cmake's configure_file,
    allowing for config options to be populated from cmake variables.
    
    Add a "reconfigure" step to the buildroot dependency that will
    update the .config in the build tree if the BUILDROOT_CONFIG_IN
    file changes.
    
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- config/buildroot/reconfigure.cmake
+++ config/buildroot/reconfigure.cmake
@@ -16,40 +16,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-if(WIN32 OR CYGWIN OR NOT ENABLE_BUILD_VMM)
-    return()
-endif()
-
-message(STATUS "Including dependency: buildroot")
-
-download_dependency(
-    buildroot
-    URL         ${BUILDROOT_URL}
-    URL_MD5     ${BUILDROOT_URL_MD5}
-)
-
 configure_file(
     ${BUILDROOT_CONFIG_IN}
     ${BUILDROOT_BUILD_DIR}/.config
     @ONLY
     NEWLINE_STYLE UNIX
-)
-
-add_dependency(
-    buildroot userspace
-    CONFIGURE_COMMAND make CC=gcc O=${BUILDROOT_BUILD_DIR} -C ${CACHE_DIR}/buildroot silentoldconfig
-    BUILD_COMMAND     make CC=gcc O=${BUILDROOT_BUILD_DIR} -C ${CACHE_DIR}/buildroot
-    INSTALL_COMMAND   ""
-)
-
-ExternalProject_Add_Step(buildroot_${USERSPACE_PREFIX} reconfigure
-    COMMAND
-        ${CMAKE_COMMAND}
-        -DBUILDROOT_CONFIG_IN=${BUILDROOT_CONFIG_IN}
-        -DBUILDROOT_BUILD_DIR=${BUILDROOT_BUILD_DIR}
-        -DLINUX_CONFIG=${LINUX_CONFIG}
-        -P ${HAVOC_SRC_CONFIG_DIR}/buildroot/reconfigure.cmake
-    DEPENDERS build
-    DEPENDEES configure
-    DEPENDS ${BUILDROOT_CONFIG_IN}
 )
