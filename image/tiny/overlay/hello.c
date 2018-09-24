fa03ae6d5c3d450f74643b64f6ae131d5488c0c8
Connor Davis <davisc@ainfosec.com>
2018-09-24 11:48:08 -0600

    Add vmcall-based hello app
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- image/tiny/overlay/hello.c
+++ image/tiny/overlay/hello.c
@@ -1,11 +1,33 @@
+#include <string.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
+
+#define STRLEN 4096
+
+char *msg[5] = {"hello", "darkness", "my", "old", "friend"};
+
+void set_str(char *str)
+{
+    static uint64_t i = 0;
+    memset(str, 0, STRLEN);
+    sprintf(str, "%s\n", msg[i++ % 5]);
+}
 
 int main()
 {
-    volatile int one = 1;
-    while (one) {
-        printf("hello\n");
-        sleep(1);
+    char str[4096];
+
+    while (1) {
+        set_str(str);
+        __asm__ volatile(
+            "xorq %%rax, %%rax\n\t"
+            "xorq %%rbx, %%rbx\n\t"
+            "movq %0, %%rax\n\t"
+            "movq %1, %%rbx\n\t"
+            "vmcall\n\t"
+            :
+            : "g"(str), "g"(STRLEN)
+            :
+        );
     }
 }
